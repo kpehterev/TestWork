ed008cc1408cda03092d2614c8d6968d6699ff03
Konstanti1n1 <116879556+Konstanti1n1@users.noreply.github.com>
2023-01-22 00:22:15 +0300

    С исправлениями

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,31 +4,48 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        printDiamond(4,13);
+
+        printDiamond(10,20);
+
+    }
+
+    static void printDiamond(int height, int width) {
+
+            String[][] romb = rombForPrtint(height, width);
+            Arrays.stream(romb).map(Arrays::toString).forEach(System.out::println);
+
     }
 
 
-    static void printDiamond(int height, int width) {
+
+    static String[][] rombForPrtint(int height, int width) {
+
+
         String[][] romb = new String[height][width];
 
         int right = width / 2;
         int left = width / 2;
         int halfH = height;
+        int delimiter = height/width;
+
 
 
         while (height > 0 && right < width && left > 0) {
             height--;
             romb[height][right++] = "#";
             romb[height][--left] = "#";
+            height-=delimiter;
 
-            if (height == halfH/2 || right == width-1 || left < 0) {
+            if (height <= halfH / 2 ) {
                 while (height > 0 && left < width && right > 0) {
                     height--;
                     romb[height][--right] = "#";
                     romb[height][left++] = "#";
+                    height-=delimiter;
                 }
             }
         }
-        Arrays.stream(romb).map(Arrays::toString).forEach(System.out::println);
+        return romb;
     }
+
 }
