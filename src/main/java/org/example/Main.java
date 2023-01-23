8d7fc89173a11155ba014b3c250c88bdc5fefebc
Konstanti1n1 <116879556+Konstanti1n1@users.noreply.github.com>
2023-01-23 14:52:06 +0300

    С исправлениями

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,44 +5,73 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        printDiamond(10,20);
+        printDiamond(10,40);
 
     }
 
     static void printDiamond(int height, int width) {
 
-            String[][] romb = rombForPrtint(height, width);
+        if(height>6){
+            String[][] romb = rombForPrtintMore(height, width);
             Arrays.stream(romb).map(Arrays::toString).forEach(System.out::println);
+        }
+
+        if(height<=6){
+            String[][] romb = rombForPrtintLess(height, width);
+            Arrays.stream(romb).map(Arrays::toString).forEach(System.out::println);
+        }
+
+
 
     }
 
 
 
-    static String[][] rombForPrtint(int height, int width) {
-
+    static String[][] rombForPrtintMore(int height, int width) {
 
         String[][] romb = new String[height][width];
 
         int right = width / 2;
         int left = width / 2;
-        int halfH = height;
+        int half = height;
         int delimiter = height/width;
 
+        while (height > 0) {
 
+            if (height > half / 2) {
 
-        while (height > 0 && right < width && left > 0) {
-            height--;
-            romb[height][right++] = "#";
-            romb[height][--left] = "#";
-            height-=delimiter;
+                height--;
+                romb[height][right++] = "#";
+                romb[height][--left] = "#";
+                height -= delimiter;
+            }else  {
+                height--;
+                romb[height][--right] = "#";
+                romb[height][left++] = "#";
+                height -= delimiter;
+            }
+        }
+        return romb;
+    }
 
-            if (height <= halfH / 2 ) {
-                while (height > 0 && left < width && right > 0) {
-                    height--;
-                    romb[height][--right] = "#";
-                    romb[height][left++] = "#";
-                    height-=delimiter;
-                }
+    static String[][] rombForPrtintLess(int height, int width) {
+
+        String[][] romb = new String[height][width];
+
+        int right = width / 2;
+        int left = width / 2;
+        int half = height;
+
+        while (height > 0 ) {
+            if (height > half / 2 && right < width && left > 0) {
+
+                height--;
+                romb[height][right++] = "#";
+                romb[height][--left] = "#";
+            }else {
+                height--;
+                romb[height][--right] = "#";
+                romb[height][left++] = "#";
             }
         }
         return romb;
