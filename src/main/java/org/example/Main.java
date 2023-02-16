db1898e0957c922eb2ae98b51588804d686a9589
Konstanti1n1 <116879556+Konstanti1n1@users.noreply.github.com>
2023-02-16 15:20:02 +0300

    Задачка с собеса

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,52 +4,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        printDiamond(4,4);
-
-    }
-
-    static void printDiamond(int height, int width) {
-
-
-            String[][] romb = rombForPrtint(height, width);
-            Arrays.stream(romb).map(Arrays::toString).forEach(System.out::println);
-
+        Person person = new Person("Sam",15);
+        System.out.println(person.toString());
 
     }
 
 
+    record Person(String name, int age){}
 
-    static String[][] rombForPrtint(int height, int width) {
-
-        String[][] romb = new String[height][width];
-
-
-        int right = width / 2;
-        int left = width / 2;
-        int half = height;
-        int delimiter;
-
-        if(height>6){
-            delimiter = height/width;
-        }
-        else delimiter=0;
-
-        while (height > 0 ) {
-            if (height > half / 2 && right < width && left > 0) {
-
-                height--;
-                romb[height][right++] = "#";
-                romb[height][--left] = "#";
-                height -= delimiter;
-            }else {
-                height--;
-                romb[height][--right] = "#";
-                romb[height][left++] = "#";
-                height -= delimiter;
-            }
-        }
-        return romb;
-    }
 
 }
